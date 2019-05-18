fc74bb058fc2ece9e88bd6839697e4a49b14f185
Wes Jonas <6207367+zeroactual@users.noreply.github.com>
2019-05-18 16:12:52 -0700

    Update kube-init.sh

--- kube-init.sh
+++ kube-init.sh
@@ -1,6 +1,6 @@
 ## Turn off swap
-swapoff -a
-sed -e '/swap/s/^/#/g' -i /etc/fstab
+sudo swapoff -a
+sudo sed -e '/swap/s/^/#/g' -i /etc/fstab
 
 ## Set up host networking
 sudo modprobe overlay
@@ -18,31 +18,31 @@ sudo sysctl --system
 # Install containerd
 ## Set up the repository
 ### Install packages to allow apt to use a repository over HTTPS
-apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo update && sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 ### Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 ### Add Docker apt repository.
-add-apt-repository \
+sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
 
 ## Install containerd
-apt-get update && apt-get install -y containerd.io
+sudo apt update && sudo apt install -y containerd.io
 
 # Configure containerd
-mkdir -p /etc/containerd
-containerd config default > /etc/containerd/config.toml
+sudo mkdir -p /etc/containerd
+sudo containerd config default > /etc/containerd/config.toml
 
 # Restart containerd
-systemctl restart containerd
+sudo systemctl restart containerd
 
 sudo apt install -y apt-transport-https curl
 
 # Install google's key
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 
 sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
