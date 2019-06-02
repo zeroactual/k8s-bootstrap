6d85d31fc35c70a34f19667dc889cadcdc5cd70a
Wes Jonas <6207367+zeroactual@users.noreply.github.com>
2019-06-01 20:19:42 -0700

    Update kube-init.sh

--- kube-init.sh
+++ kube-init.sh
@@ -67,6 +67,6 @@ kubeadm init --pod-network-cidr=192.168.0.0/16
 
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+chown -R `whoami` $HOME/.kube
 
 kubectl taint nodes --all node-role.kubernetes.io/master-
